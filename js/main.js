e057605074bf577095f40c23b74160d32b0417ae
Carlos Alcalá <CAlcala@audalia.com>
2016-07-01 15:00:14 +0200

    about page, responsive menu

--- js/main.js
+++ js/main.js
@@ -1,20 +1,27 @@
 // Variables
 var
-    $menuItemSquare = $('.menuItemSquare'),
-    $photoThumb = $('.photoThumb'),
+    $menuItemSquare     = $('.menuItemSquare'),
+    $photoThumb         = $('.photoThumb'),
+    $showMenuButton     = $('.showMenuButton'),
+    $menu               = $('.sideMenu ul'),
+    $menuItem           = $('.sideMenu .sm-item'),
     linkLocation
 
 
+//Events
 $menuItemSquare.hover(reduceSiblingsOpacity,resetSiblingsOpacity)
+$menuItemSquare.on("click", openMenuLink)
+$showMenuButton.on("click", toggleMenu)
+$menuItem.on("click", navigateTo)
 
+
+// Functions
 function reduceSiblingsOpacity () {
     $(this).siblings().addClass("lowOpacity")
 }
 function resetSiblingsOpacity () {
     $(this).siblings().removeClass("lowOpacity")
 }
-
-$menuItemSquare.on("click", openMenuLink)
 
 function openMenuLink(event){
     event.preventDefault()
@@ -26,4 +33,26 @@ function openMenuLink(event){
     setTimeout(function(){
          window.location = linkLocation
     },300)
+}
+
+
+function toggleMenu(){
+    $showMenuButton.stop().toggleClass("rotated")
+    $menu.stop().slideToggle()
+}
+
+function navigateTo(){
+
+    if($showMenuButton.hasClass("rotated")){
+        event.preventDefault()
+        linkLocation = $(this).children("a").attr("href")
+
+        $showMenuButton.stop().toggleClass("rotated")
+        $menu.stop().slideToggle(300, function(){
+            setTimeout(function(){
+                 window.location = linkLocation
+            },100)
+        })
+    }
+
 }
