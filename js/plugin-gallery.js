b268fa10e17d817eff019724070dfc4f4ad8baa8
Carlos Alcalá <Carlos@Mac-mini-de-Carlos.local>
2016-07-24 21:15:31 +0200

    finished gallery plugin

--- js/plugin-gallery.js
+++ js/plugin-gallery.js
@@ -16,6 +16,12 @@ var currentPhoto = 0
 
 // Events
 $images.on('click', openGalleryView)
+$body.on("click", ".veil" , closeCustAlert)
+$body.on("click", ".btnClose" , closeCustAlert)
+$body.on("click", ".btnNextPhoto" , showNext)
+$body.on("click", ".btnPrevPhoto" , showPrev)
+$body.on("click", ".dialog" , stopPropagation)
+
 
 init()
 
@@ -23,15 +29,67 @@ function init (){
     $images.each(function(){
         photoList.push($(this).attr('src'))
     })
-    console.log(photoList.length)
 }
 
 function openGalleryView(event){
     event.preventDefault()
     currentPhoto = $images.index($(this))
 
-    $alertCust = $('<div class="veil"><div class="dialog"><img src="'+photoList[currentPhoto]+'"></div></div>').fadeIn()
+    $alertCust = $('<div class="veil"><div class="dialog"><img src="'+photoList[currentPhoto]+'"><div class="btnNextPhoto"></div><div class="btnPrevPhoto"></div><div class="btnClose"></div></div></div>').fadeIn()
 
     $alertCust.appendTo($body)
 
+    checkVisibleButtons ()
+
+}
+
+
+function showNext () {
+    currentPhoto+=1
+    $('.dialog img').fadeOut(400,function(){
+        $(this).attr('src', photoList[currentPhoto]).fadeIn(200)
+    })
+
+    checkVisibleButtons ()
+}
+
+function showPrev() {
+    currentPhoto-=1
+   $('.dialog img').fadeOut(400,function(){
+        $(this).attr('src', photoList[currentPhoto]).fadeIn(200)
+    })
+
+   checkVisibleButtons ()
+}
+
+function closeCustAlert () {
+
+    $(".veil").fadeOut(400 ,
+        function() {
+            $(this).remove()
+        }
+    )
+}
+
+
+function stopPropagation (event){
+    event.stopPropagation();
+}
+
+function checkVisibleButtons () {
+
+    console.log (currentPhoto)
+    // Show/Hide next button
+    if (currentPhoto == photoList.length-1){
+        $('.btnNextPhoto').css("display","none")
+    }else{
+        $('.btnNextPhoto').css("display","block")
+    }
+
+    // Show/Hide prev button
+    if (currentPhoto == 0){
+        $('.btnPrevPhoto').css("display","none")
+    }else{
+        $('.btnPrevPhoto').css("display","block")
+    }
 }
